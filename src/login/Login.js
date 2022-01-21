d553560058922496c726be6364e3f51acb4b2af5
Luis Torres <luisdtor@gmail.com>
2022-01-21 16:17:43 -0600

    Created basic layout for login component

--- src/login/Login.js
+++ src/login/Login.js
@@ -2,7 +2,22 @@ import React from "react";
 import "./Login.css";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="login-container">
+      <h3>Login</h3>
+      <form>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
