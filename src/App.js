758ec76f62f1d7f7062a1fc9336ad83e24508b21
Luis Torres <luisdtor@gmail.com>
2022-03-01 09:52:25 -0600

    Created a Home component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
+import HomeView from "./home/HomeView";
 import LoginView from "./login/LoginView";
 import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  const [token, setToken] = useState(null);
+
   return (
     <div className="App">
       <Routes>
-        <Route exact path="/" element={<LoginView />} />
+        <Route exact path="/" element={<LoginView />}></Route>
       </Routes>
     </div>
   );
---
+++ src/home/HomeView.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HomeView.css";
+
+function HomeView() {
+  return (
+    <div>
+      <h1>Home View</h1>
+    </div>
+  );
+}
+
+export default HomeView;
