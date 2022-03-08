4ea216c97a9c7d23884938e76cf8757a3fd308d0
Luis Torres <luisdtor@gmail.com>
2022-03-08 10:57:14 -0600

    Implemented auto-login/auto-logout

--- src/App.js
+++ src/App.js
@@ -2,15 +2,45 @@ import "./App.css";
 import HomeView from "./home/HomeView";
 import LoginView from "./login/LoginView";
 import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
-import { useCallback, useState } from "react";
-import NotFound from "./common/NotFound";
+import { useCallback, useEffect, useState } from "react";
 import { AuthContext } from "./common/authContext";
+
+let logoutTimer;
 
 function App() {
   const navigate = useNavigate();
   const [token, setToken] = useState(null);
   const [userEmail, setUserEmail] = useState(null);
   const [userId, setUserId] = useState(null);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState(null);
+
+  useEffect(() => {
+    const userData = JSON.parse(localStorage.getItem("puppiesUserData"));
+    if (userData && userData.token && new Date(userData.expiration) > new Date()) {
+      setToken(userData.token);
+      setUserEmail(userData.email);
+      setUserId(userData.userId);
+      setTokenExpirationDate(new Date(userData.expiration));
+    }
+  }, []);
+
+  const logout = useCallback(() => {
+    setToken(null);
+    setUserEmail(null);
+    setUserId(null);
+    setTokenExpirationDate(null);
+    localStorage.removeItem("puppiesUserData");
+    navigate("auth");
+  }, [navigate]);
+
+  useEffect(() => {
+    if (token && tokenExpirationDate) {
+      const remainingTime = tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [token, logout, tokenExpirationDate]);
 
   const login = useCallback(async (formData) => {
     try {
@@ -29,6 +59,12 @@ function App() {
         setToken(res.data.token);
         setUserEmail(res.data.email);
         setUserId(res.data.userId);
+        const tokenExpirationDate = new Date(new Date().getTime() + 1000 * 60 * 60);
+        setTokenExpirationDate(tokenExpirationDate);
+        localStorage.setItem(
+          "puppiesUserData",
+          JSON.stringify({ ...res.data, expiration: tokenExpirationDate.toISOString() })
+        );
       } else {
         throw res.error;
       }
@@ -54,6 +90,12 @@ function App() {
         setToken(res.data.token);
         setUserEmail(res.data.email);
         setUserId(res.data.userId);
+        const tokenExpirationDate = new Date(new Date().getTime() + 1000 * 60 * 60);
+        setTokenExpirationDate(tokenExpirationDate);
+        localStorage.setItem(
+          "puppiesUserData",
+          JSON.stringify({ ...res.data, expiration: tokenExpirationDate.toISOString() })
+        );
       } else {
         throw res.error;
       }
@@ -61,13 +103,6 @@ function App() {
       window.alert(error + ". Please try again.");
     }
   }, []);
-
-  const logout = useCallback(() => {
-    setToken(null);
-    setUserEmail(null);
-    setUserId(null);
-    navigate("auth");
-  }, [navigate]);
 
   let routes;
 
@@ -82,7 +117,7 @@ function App() {
     routes = (
       <>
         <Route path="auth" element={<LoginView />}></Route>
-        <Route path="*" element={<NotFound />}></Route>
+        <Route path="*" element={<Navigate to="auth" />}></Route>
       </>
     );
   }
